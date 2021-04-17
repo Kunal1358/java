2cdcefd9c6d5fd4f07f392f42a26ae6e9a9ab1a5
Kunal1358 <kunalkohli8054@gmail.com>
2021-04-17 19:45:38 +0530

    Code Updated

--- src/Practice/day1/basics/Java_Variables.java
+++ src/Practice/day1/basics/Java_Variables.java
@@ -53,37 +53,4 @@ class A{
         }
 }
 
-* Data Types *
-
-There are 2 data types
-
-Primitive     -> byte,int,short,float etc
-non primitive -> classes,interface and arrays
-
-* * java uses Unicode system not ASCII code system. The \u0000 is the lowest range of Unicode system. * *
-
-
-A particular code value corresponds to different letters in the various language standards.
-The encodings for languages with large character sets have variable length.Some common characters
-are encoded as single bytes, other require two or more byte.
-
-To solve these problems, a new language standard was developed i.e. Unicode System.
-In unicode, character holds 2 byte, so java also uses 2 byte for characters.
-lowest value:\u0000
-highest value:\uFFFF
-
-* Float vs Double *
-
-float  ->  single precision - 6 decimal digits  - low accuracy
-double ->  double precision - 15 decimal digits - high accuracy
-
-Default Data Type
-	Float  -> Java does not use it as the default floating-point number.
-    Double -> It is the default data type for floating-point numbers.
-
-Suffix
-    Float  -> It uses F or f as a suffix. It is mandatory to add a suffix if you are declaring a float variable.
-    Double -> It uses d or D as a suffix. It is optional to add a suffix if you are declaring a double variable.
-
-
  */
