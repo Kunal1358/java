67c933d10c6dcb4a337e6d3bd09eb80b9a34ac08
Kunal1358 <kunalkohli8054@gmail.com>
2021-04-29 11:17:38 +0530

    Day 5 -> Aggregation, Method overloading and overriding

---
+++ src/Practice/day5/Aggregation/Example/Emp.java
@@ -0,0 +1,34 @@
+package Practice.day5.Aggregation.Example;
+
+public class Emp {
+    int id;
+    String name;
+    Address address; // Aggregation
+
+    public Emp(int id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    void print(){
+
+        System.out.println(id +"  " + name);
+
+        System.out.println(address.city +"  " +address.state + "  " + address.country );
+
+    }
+
+    public static void main(String[] args) {
+        Address address1=new Address("kalka","pinjore","Haryana");
+        Address address2=new Address("Chandigarh","Mohali","Punjab");
+
+        Emp e1=new Emp( 1 , "Kunal",address1);
+        Emp e2=new Emp( 2 , "Raj",address2);
+
+        e1.print();
+        e2.print();
+
+    }
+
+}
