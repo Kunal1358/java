7cc27ec73b786ff8ce4b4c3438429898eedf2f94
Kunal1358 <kunalkohli8054@gmail.com>
2021-04-29 16:54:34 +0530

    Covariant Return Type, Super Keyword, Instance initializer block, Final Keyword, Polymorphism, Binding,  abstract class, Interface

---
+++ src/Practice/day6/Interface/Multiple_inheritance/Interface_inheritance.java
@@ -0,0 +1,28 @@
+package Practice.day6.Interface.Multiple_inheritance;
+
+interface printable{
+    void print();
+}
+
+interface showable extends printable{
+    void show();
+}
+
+class call implements showable{
+    public void print(){
+        System.out.println("Printed");
+    }
+    public void show(){
+        System.out.println("showing");
+    }
+}
+
+public class Interface_inheritance {
+
+    public static void main(String[] args) {
+        call c = new call();
+        c.print();
+        c.show();
+    }
+
+}
---
+++ src/Practice/day6/Polymorphism/Run_time_Polymorphism/Code1.java
@@ -0,0 +1,31 @@
+package Practice.day6.Polymorphism.Run_time_Polymorphism;
+
+class  Bike{
+
+    int speed=100;
+}
+
+class honda extends Bike{
+    int speed=800;
+}
+
+public class Code1 {
+    public static void main(String[] args) {
+        Bike b=new honda();
+        System.out.println(b.speed);
+
+    }
+
+    // methods -> object
+    // data members -> reference
+
+
+}
+
+/*
+
+Since we are accessing the data member which is not overridden, hence it will access the data member of the Parent class always.
+
+Rule: Runtime polymorphism can't be achieved by data me
+
+ */
